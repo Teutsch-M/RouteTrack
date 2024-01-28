00558b29abdea7c4edb1033373748bbde3e66c33
Teutsch-M <mlteutsch@gmail.com>
2024-01-29 01:23:15 +0200

    adding typeconverter for bitmap, adding fragments

---
+++ app/src/main/java/com/example/routetrack/database/Converter.kt
@@ -0,0 +1,19 @@
+package com.example.routetrack.database
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import java.io.ByteArrayOutputStream
+
+class Converter {
+
+    fun fromBitmap(bmp: Bitmap): ByteArray{
+        val output = ByteArrayOutputStream()
+        bmp.compress(Bitmap.CompressFormat.PNG,100,output)
+        return output.toByteArray()
+    }
+
+    fun toBitmap(byteArray: ByteArray): Bitmap{
+        return BitmapFactory.decodeByteArray(byteArray,0,byteArray.size)
+    }
+
+}
